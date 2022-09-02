92b79017270eacacc2dc6090319a5bbbed11efdc
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-09-02 03:09:04 +0300

    Modify JS and style modified change using CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     image = document.getElementById('resulting-image'),
     movieDetails = document.getElementById("movie-details-result")
 
+    // Default Movie and it's details
     fetch('http://www.omdbapi.com/?i=tt3896198&apikey=d6424164')
     .then(response => response.json())
     .then(object => {
@@ -12,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
         movieDetails.innerHTML = 
         `
         <p id="movie-info"> 
-            <span>Title</span> : &emsp;&emsp;&emsp;&emsp; <i>${object.Title}</i> <br>
+            <span>Title</span> : &emsp;&emsp;&emsp;&emsp; <strong><i>${object.Title}</i></strong> <br>
             <span>Type</span> : &emsp;&emsp;&emsp;&emsp; <i>${object.Type}</i> <br>
             <span>Genre</span> : &emsp;&emsp;&emsp; <i>${object.Genre}</i> <br>
             <span>Release date</span> : &nbsp; <i>${object.Released}</i> <br>
@@ -22,12 +23,13 @@ document.addEventListener('DOMContentLoaded', () => {
             <span>Writer(s)</span> : &emsp;&emsp;&nbsp; <i>${object.Writer}</i> <br>
             <span>Actors</span> : &emsp;&emsp;&emsp; <i>${object.Actors}</i> <br>
             <span>Plot</span> : <br> ${object.Plot} <br>
+            <span>Awards</span> : <br> ${object.Awards} <br> 
             <span>Ratings</span> : 
                     <ul>
                         <li><i>IMDB - &nbsp; ${object.Ratings[0].Value}</i></li>
                         <li><i>Rotten Tomatoes - &nbsp; ${object.Ratings[1].Value}</i></li>
                         <li><i>Metacritic - &nbsp; ${object.Ratings[2].Value}</i></li>                        
-                    </ul> 
+                    </ul> <br>
         </p>
         `
 
