7bb94a1feb904ca5ff2b693ddd1a5b95f46ce218
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-09-05 00:36:11 +0300

    Modify JS. Add event listener for submit to first form. Add a function to fetch data from API when the event occurs.

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
     addedComments = document.getElementById('added-comments')
 
     // Default Movie and it's details
-    fetch('http://www.omdbapi.com/?i=tt3896198&apikey=d6424164')
+
+    fetch(`http://www.omdbapi.com/?t=the+mask&plot=full&apikey=d6424164`)
     .then(response => response.json())
     .then(object => {
         image.removeAttribute('src')
@@ -34,7 +35,72 @@ document.addEventListener('DOMContentLoaded', () => {
         </p>
         `
     })
-    .catch(error => error)
+    .catch(error =>  {
+        document.getElementById("search-results"). innerText = `${error} Please refresh page and try again`
+    })
+
+    searchForm.addEventListener('submit', e => {
+        e.preventDefault()
+        resultsForSearch(e.target.movie_title.value)
+        searchForm.reset()
+    })
+    function resultsForSearch(string) {
+        fetch(`http://www.omdbapi.com/?t=${string}&plot=full&apikey=d6424164`)
+        .then(response => response.json())
+        .then(object => {
+            image.removeAttribute('src')
+            image.setAttribute('src', `${object.Poster}`)
+            if (object.Type == "movie") {
+                image.removeAttribute('src')
+                image.setAttribute('src', `${object.Poster}`)
+                movieDetails.innerHTML = 
+            `
+            <p id="movie-info"> 
+                <span>Title</span> : &emsp;&emsp;&emsp;&emsp; <strong><i>${object.Title}</i></strong> <br>
+                <span>Type</span> : &emsp;&emsp;&emsp;&emsp; <i>${object.Type}</i> <br>
+                <span>Genre</span> : &emsp;&emsp;&emsp; <i>${object.Genre}</i> <br>
+                <span>Release date</span> : &nbsp; <i>${object.Released}</i> <br>
+                <span>Rated</span> : &emsp;&emsp;&emsp;&nbsp; <i>${object.Rated}</i>  <br>
+                <span>Duration</span> : &emsp;&emsp;&nbsp; <i>${object.Runtime}</i> <br>
+                <span>Director(s)</span> : &emsp;&emsp; <i>${object.Director}</i> <br>
+                <span>Writer(s)</span> : &emsp;&emsp;&nbsp; <i>${object.Writer}</i> <br>
+                <span>Actors</span> : &emsp;&emsp;&emsp; <i>${object.Actors}</i> <br>
+                <span>Plot</span> : <br> ${object.Plot} <br>
+                <span>Awards</span> : <br> ${object.Awards} <br> 
+                <span>Ratings</span> : 
+                <ul>
+                    <li><i>IMDB - &nbsp; ${object.Ratings[0].Value}</i></li>
+                    <li><i>Rotten Tomatoes - &nbsp; ${object.Ratings[1].Value}</i></li>
+                    <li><i>Metacritic - &nbsp; ${object.Ratings[2].Value}</i></li>                        
+                </ul> <br>
+            </p>
+            `
+            } else if (object.Type == 'series'){
+                image.removeAttribute('src')
+                image.setAttribute('src', `${object.Poster}`)
+                movieDetails.innerHTML = 
+                `
+                <p id="movie-info"> 
+                    <span>Title</span> : &emsp;&emsp;&emsp;&emsp; <strong><i>${object.Title}</i></strong> <br>
+                    <span>Type</span> : &emsp;&emsp;&emsp;&emsp; <i>${object.Type}</i> <br>
+                    <span>Genre</span> : &emsp;&emsp;&emsp; <i>${object.Genre}</i> <br>
+                    <span>Release date</span> : &nbsp; <i>${object.Released}</i> <br>
+                    <span>Year(s)</span> : &emsp;&emsp;&emsp;&nbsp; <i>${object.Year}</i> <br>
+                    <span>Rated</span> : &emsp;&emsp;&emsp;&nbsp; <i>${object.Rated}</i>  <br>
+                    <span>Director(s)</span> : &emsp;&emsp; <i>${object.Director}</i> <br>
+                    <span>Writer(s)</span> : &emsp;&emsp;&nbsp; <i>${object.Writer}</i> <br>
+                    <span>Actors</span> : &emsp;&emsp;&emsp; <i>${object.Actors}</i> <br>
+                    <span>Ratings</span> : &emsp;&emsp; <i>IMDB - &nbsp; ${object.Ratings[0].Value}</i> <br>
+                    <span>Plot</span> : <br> ${object.Plot} <br>
+                    <span>Awards</span> : <br> ${object.Awards} <br> 
+                </p>
+                `
+            }
+        })
+        .catch(error =>  {
+            document.getElementById("search-results"). innerHTML = `<p> <b>${error.message}. Please refresh page and try again </b> </p>`
+        })
+    }
 
     // commentsForm.addEventListener('submit', e => {
     //     e.preventDefault()
